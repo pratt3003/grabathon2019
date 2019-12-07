8f92c6b242a468103d7d6a19c969f606ff44c285
Aditya Sharma <adityasharma.code@gmail.com>
2019-12-07 22:45:54 +0530

    liquibase

--- src/main/java/com/grabathon/booster/BoosterApplication.java
+++ src/main/java/com/grabathon/booster/BoosterApplication.java
@@ -2,6 +2,10 @@ package com.grabathon.booster;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 @SpringBootApplication
 public class BoosterApplication {
@@ -10,4 +14,29 @@ public class BoosterApplication {
     SpringApplication.run(BoosterApplication.class, args);
   }
 
+  /**
+   * Validation Message Source Bean.
+   *
+   * @return
+   */
+  @Bean
+  public MessageSource validationMessageSource() {
+    ReloadableResourceBundleMessageSource bean = new ReloadableResourceBundleMessageSource();
+    bean.setAlwaysUseMessageFormat(true);
+    bean.setBasename("classpath:validation-messages");
+    bean.setDefaultEncoding("UTF-8");
+    return bean;
+  }
+
+  /**
+   * Validator Factory Bean.
+   *
+   * @return
+   */
+  @Bean
+  public LocalValidatorFactoryBean validator() {
+    LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+    bean.setValidationMessageSource(validationMessageSource());
+    return bean;
+  }
 }
--- src/main/java/com/grabathon/booster/model/Message.java
+++ src/main/java/com/grabathon/booster/model/Message.java
@@ -1,28 +1,23 @@
 package com.grabathon.booster.model;
 
+import java.io.Serializable;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import lombok.Data;
 
 @Data
 @Entity
-public class Message {
+@Table(name = "messages")
+public class Message implements Serializable {
 
   @Id
+  @Column(name = "id", nullable = false)
   @GeneratedValue(strategy = GenerationType.SEQUENCE)
   private int id;
-
-  private String message;
-
-  public String getMessage() {
-    return message;
-  }
-
-  public void setMessage(String message) {
-    this.message = message;
-  }
-
 }
