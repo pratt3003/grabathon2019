96ff5a0495a2fefc69bcf460826ef033402c2c92
Aditya Sharma <adityasharma.code@gmail.com>
2019-12-07 19:57:50 +0530

    added repo and model

--- src/main/java/com/grabathon/booster/controller/HelloWorldController.java
+++ src/main/java/com/grabathon/booster/controller/HelloWorldController.java
@@ -1,11 +1,15 @@
 package com.grabathon.booster.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.grabathon.booster.model.Message;
+import com.grabathon.booster.repository.HelloWorldRepository;
 
 @RestController
 @RequestMapping("/hello")
@@ -13,11 +17,12 @@ public class HelloWorldController {
 
   private static final String MESSAGE = "Hello, World!";
 
+  @Autowired
+  private HelloWorldRepository helloWorldRepository;
+
   @GetMapping(produces = { MediaType.APPLICATION_JSON_VALUE })
-  public Message hello() {
-    Message message = new Message();
-    message.setMessage(MESSAGE);
-    return message;
+  public List<Message> hello() {
+    return helloWorldRepository.findAll();
   }
 
 }
