10bf36b1c550d9c65e79c252b2e7513a7a19be26
Aditya Sharma <adityasharma.code@gmail.com>
2019-12-08 06:15:48 +0530

    added migration for lat long

--- src/main/java/com/grabathon/booster/model/Merchant.java
+++ src/main/java/com/grabathon/booster/model/Merchant.java
@@ -1,13 +1,10 @@
 package com.grabathon.booster.model;
 
-import java.awt.Point;
 import java.io.Serializable;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -21,7 +18,9 @@ public class Merchant extends AbstractBaseEntity implements Serializable {
   @Column(name = "name", nullable = false, length = 50)
   private String name;
 
-  @JsonIgnore
-  @Column(name = "location", columnDefinition = "Point")
-  private Point location;
+  @Column(name = "latitude")
+  private Float latitude;
+
+  @Column(name = "longitude")
+  private Float longitude;
 }
